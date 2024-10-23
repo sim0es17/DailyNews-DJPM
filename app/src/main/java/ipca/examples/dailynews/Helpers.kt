c9282ba8c1bf1259bde27288b14eb1985110e17c
simoes17 <bentosimoes1@gmail.com>
2024-10-23 09:48:49 +0100

    Change API Key

---
+++ app/src/main/java/ipca/examples/dailynews/Helpers.kt
@@ -0,0 +1,24 @@
+package ipca.examples.dailynews
+
+import java.net.URLEncoder
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+fun String.parseDate() : Date? {
+    //2024-10-14T08:20:01Z
+    val pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+    return SimpleDateFormat(pattern, Locale.getDefault())
+        .parse(this)
+}
+
+fun String.encodeURL() : String {
+    return URLEncoder.encode(this, "UTF-8")
+}
+
+fun Date.toYYYYMMDD() : String {
+    val pattern = "yyyy-MM-dd"
+    return SimpleDateFormat(pattern, Locale.getDefault())
+        .format(this)
+}
+
---
+++ app/src/main/java/ipca/examples/dailynews/MainActivity.kt
@@ -0,0 +1,52 @@
+package ipca.examples.dailynews
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import ipca.examples.dailynews.ui.ArticleDetail
+import ipca.examples.dailynews.ui.HomeView
+import ipca.examples.dailynews.ui.theme.DailyNewsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DailyNewsTheme {
+                var navController = rememberNavController()
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    NavHost(navController = navController,
+                        startDestination = Screen.Home.route ) {
+                        composable(route = Screen.Home.route) {
+                            HomeView(
+                                navController = navController,
+                                modifier = Modifier.padding(innerPadding)
+                            )
+                        }
+                        composable(route = Screen.ArticleDetail.route) {
+                            val url = it.arguments?.getString("articleUrl")
+                            ArticleDetail(
+                                modifier = Modifier.padding(innerPadding),
+                                url = url ?: ""
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+sealed class Screen(val route: String) {
+    object Home : Screen("home")
+    object ArticleDetail : Screen("article_detail/{articleUrl}")
+
+}
